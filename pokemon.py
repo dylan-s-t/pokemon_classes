776854a86d912d9a4ddd18fb0616329eceb10fc3
Dylan <dylantrerise@gmail.com>
2020-01-02 02:05:15 -0800

    added methods to pokemon and trainer classes, and added test class instances

--- pokemon.py
+++ pokemon.py
@@ -14,9 +14,10 @@ class Pokemon:
       self.knock_out()
     print("{name} now has {health} health".format(name = self.name, health = self.health))
       
-  
   def regain_health(self, health_gain):
     self.health += health_gain
+    if self.health > self.max_health:
+      self.health = self.max_health
     print("{name} now has {health} health".format(name = self.name, health = self.health))
   
   def knock_out(self):
@@ -29,6 +30,55 @@ class Pokemon:
       print("{name} is revived!".format(name = self.name))
     else:
       print("{name} is not knocked out and does not need reviving!".format(name = self.name))
+      
+  def attack(self, other_poke):
+    other_type = other_poke.type
+    if self.type == "Fire" and other_type == "Grass":
+      damage_mult = 2
+    elif self.type == "Grass" and other_type == "Fire":
+      damage_mult = 0.5
+    elif self.type == "Grass" and other_type == "Water":
+      damage_mult = 2
+    elif self.type == "Water" and other_type == "Grass":
+      damage_mult = 0.5
+    elif self.type == "Water" and other_type == "Fire":
+      damage_mult = 2
+    elif self.type == "Fire" and other_type == "Water":
+      damage_mult = 0.5
+      
+    damage_dealt = damage_mult * self.level
+    print("{attack_poke} deals {damage_dealt} to {def_poke}!".format(attack_poke = self.name, damage_dealt = damage_dealt, def_poke = other_poke.name))
+    pokemon.lose_health(damage_dealt)
     
+class Trainer:
+  def __init__(self, name, potions, pokemons, current_pokemon):
+    self.name = name
+    self.potions = potions
+    if len(pokemons) <= 6:
+      self.pokemons = pokemons
+    self.current_pokemon = current_pokemon
     
+  def use_potion(self, potion):
+    if self.potions.get(potion):
+      poke_to_heal = self.pokemons[self.current_pokemon]
+      poke_to_heal.regain_health(self.potions[potion])
+      if poke_to_heal.health > poke_to_heal.max_health:
+        poke_to_heal.health = poke_to_heal.max_health
+      print("{name} was healed by {potion} for {heal_hp} hp and now has {health} hp".format(name = poke_to_heal.name, potion = potion, heal_hp = self.potions[potion], health = poke_to_heal.health))
   
+  def attack_trainer(self, other_trainer):
+    their_pokemon = other_trainer.pokemons[other_trainer.current_pokemon]
+    attack_poke = self.pokemons[self.current_pokemon]
+    attack_poke.attack(their_pokemon)
+    print("{trainer}'s {attack_poke} has attacked {other trainer}'s {their_pokemon}".format(trainer = self.name, attake_poke = attack_poke, other_trainer = other_trainer, their_pokemon = their_pokemon))
+
+charzard = Pokemon("Charzard", 20, "Fire", 3)
+blastoise = Pokemon("Blastoise", 100, "Water", 10)
+leafeon = Pokemon("Leafeon", 79, "Grass", 9)
+bulbasaur = Pokemon("Bulbasaur", 40, "Water", 4)
+
+dylan = Trainer("Dylan", {"healx": 20, "healy": 40}, [charzard, blastoise], 0)
+
+nick = Trainer("Nick", {"healz": 50}, [leafeon, bulbasaur], 0)
+
+nick.use_potion("healz")
